4f967d222f849e57808f5963e0ac2d70a4143f00
Twillij <silentwill29@gmail.com>
2021-04-08 18:42:54 +1000

    Added material changer script

---
+++ Assets/Scripts/MaterialChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+    public GameObject target;
+    public List<Material> materials;
+
+    private int matIndex = 0;
+
+    private void SwitchMaterial()
+    {
+        matIndex = (matIndex + 1 < materials.Count) ? matIndex + 1 : 0;
+
+        target.GetComponent<Renderer>().material = materials[matIndex];
+    }
+
+    private void Start()
+    {
+        if (!target)
+            target = gameObject;
+    }
+}
